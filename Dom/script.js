3f34c124e8ae3105145d79b5f7257bbc54721730
Kevin-BV <Kevin.basoalto.v@gmail.com>
2025-08-27 17:17:34 -0400

    222

--- Dom/script.js
+++ Dom/script.js
@@ -1,2 +1,11 @@
-nombre = promt ("Ingrese su nombre")
+titulo = document.querySelector("h3")
+titulo.innerHTML = "Este contenido fue agregado con JavaScript"
+
+
+
+
+/*nombre = prompt ("Ingrese su nombre")
+edad = prompt ("Ingrese edad: ")
+edadfinal= Number (edad) + 10
 alert("Hola " + nombre)
+console.log ("tendrás en 10 años, esta edad:", edadfinal)*/
